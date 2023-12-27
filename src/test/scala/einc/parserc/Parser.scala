f25b6dc562be19481e0ea0a0614d1a2c8a2271c3
Yichen Xu <yichen.x@outlook.com>
2023-12-27 23:12:45 +0100

    Add a test

--- src/test/scala/einc/parserc/Parser.scala
+++ src/test/scala/einc/parserc/Parser.scala
@@ -1,8 +1,10 @@
 package einc.parserc
 
+import Parser.{*, given}
 import Parsers.*
 
 class ParserSuite extends munit.FunSuite:
   test("parser: predicate"):
     val p = predicate(_ == 'a')
-
+    assert("abc".parseWith(p).getResult == 'a')
+    assert("bc".parseWith(p).isError)
