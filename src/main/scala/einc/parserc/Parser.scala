3bfd495fcc2e896439bad9e2d2646c763c47782c
Yichen Xu <yichen.x@outlook.com>
2023-12-27 23:11:06 +0100

    Use a union type for parsing results

--- src/main/scala/einc/parserc/Parser.scala
+++ src/main/scala/einc/parserc/Parser.scala
@@ -7,14 +7,16 @@ case class ParseError(pos: SourcePos, msg: String, isRecoverable: Boolean = fals
   def toRecoverable: ParseError = copy(isRecoverable = true)
   def toFatal: ParseError = copy(isRecoverable = false)
 
+type ParseResult[X] = ParseError | (InputState, X)
+
 /** A parser combinator. */
 trait Parser[+X]:
-  def run(input: InputState): Either[ParseError, (InputState, X)]
+  def run(input: InputState): ParseResult[X]
 
   def mapError(f: ParseError => ParseError): Parser[X] = Parser: input =>
     run(input) match
-      case Left(err) => Left(f(err))
-      case Right(r) => Right(r)
+      case err: ParseError => f(err)
+      case (input1, x) => (input1, x)
 
   def withError(err: ParseError): Parser[X] = mapError(_ => err)
 
@@ -23,31 +25,36 @@ trait Parser[+X]:
   def fatal: Parser[X] = mapError(_.toFatal)
 
 object Parser:
-  def apply[X](f: InputState => Either[ParseError, (InputState, X)]) = new Parser[X]:
-    def run(input: InputState): Either[ParseError, (InputState, X)] = f(input)
+  def apply[X](f: InputState => ParseResult[X]) = new Parser[X]:
+    def run(input: InputState): ParseResult[X] = f(input)
 
   def fail(msg: String, isRecoverable: Boolean = false): Parser[Nothing] = Parser: input =>
-    Left(ParseError(input.pos, msg, isRecoverable))
+    ParseError(input.pos, msg, isRecoverable)
 
   def flatten[A](ppa: Parser[Parser[A]]): Parser[A] = Parser: input =>
-    ppa.run(input).flatMap: (input1, pa) =>
-      pa.run(input1)
+    ppa.run(input) match
+      case (input1, pa) =>
+        pa.run(input1)
+      case err: ParseError => err
 
   given parserIsApplicative: Applicative[Parser] with
     def pure[X](x: X): Parser[X] = Parser: input =>
-      Right(input, x)
+      (input, x)
 
     extension [A](fa: Parser[A])
       def map[B](op: A => B): Parser[B] = Parser: input =>
         fa.run(input) match
-          case Left(err) => Left(err)
-          case Right((o, x)) => Right((o, op(x)))
+          case err: ParseError => err
+          case (o, x) => (o, op(x))
 
     extension [A, B](ff: Parser[A => B])
       def <*>(fa: => Parser[A]): Parser[B] = Parser: input =>
-        ff.run(input).flatMap: (input1, f) =>
-          fa.run(input1).map: (input2, x) =>
-            (input2, f(x))
+        ff.run(input) match 
+          case (input1, f) =>
+            fa.run(input1) match 
+              case (input2, x) => (input2, f(x))
+              case err: ParseError => err
+          case err: ParseError => err
 
   given parserIsMonad: Monad[Parser] with
     def unit[X](x: X): Parser[X] = x.embed
@@ -61,7 +68,30 @@ object Parser:
     extension [A](fa: Parser[A]) 
       def <|>[B](fb: => Parser[B]): Parser[A | B] = Parser: input =>
         fa.run(input) match
-          case Left(err) =>
+          case err: ParseError =>
             if err.isRecoverable then fb.run(input)
-            else Left(err)
-          case Right((o, x)) => Right((o, x))
+            else err
+          case x => x
+
+  extension [X](res: ParseResult[X])
+    def getError: ParseError = res match
+      case err: ParseError => err
+      case _ => assert(false, "ParseResult is not an error")
+
+    def getResult: X = res match
+      case (_, x) => x
+      case _ => assert(false, "ParseResult is an error")
+
+    def getInputState: InputState = res match
+      case (input, _) => input
+      case _ => assert(false, "ParseResult is an error")
+
+    def isOk: Boolean = res match
+      case _: ParseError => false
+      case _ => true
+
+    def isError: Boolean = !isOk
+
+  extension (s: String)
+    def parseWith[X](p: Parser[X]): ParseResult[X] =
+      p.run(InputState.fromString(s))
--- src/main/scala/einc/parserc/Parsers.scala
+++ src/main/scala/einc/parserc/Parsers.scala
@@ -7,22 +7,13 @@ import MonadOps.*
 import Parser.given
 
 object Parsers:
-  def isEof: Parser[Boolean] = Parser:
-    case i @ InputState(str, pos) =>
-      Right(i, pos.idx >= str.length)
-
-  def peek: Parser[Char] = Parser:
-    case i @ InputState(str, pos) =>
-      if pos.idx >= str.length then Left(ParseError(pos, "unexpected EOF"))
-      else Right((i, str(pos.idx)))
-
   def predicate(p: Char => Boolean, msg: String | Null = null): Parser[Char] = Parser:
     case i @ InputState(str, pos) if pos.idx >= str.length =>
-      Left(ParseError(pos, "unexpected EOF"))
+      ParseError(pos, "unexpected EOF")
     case i @ InputState(str, pos) if p(str(pos.idx)) =>
-      Right(InputState(str, pos.next), str(pos.idx))
+      (InputState(str, pos.next), str(pos.idx))
     case i =>
-      Left(ParseError(i.pos, if msg ne null then msg else "unmatched predicate"))
+      ParseError(i.pos, if msg ne null then msg else "unmatched predicate")
 
   def char(ch: Char): Parser[Unit] = predicate(_ == ch) #> ()
 
@@ -30,7 +21,11 @@ object Parsers:
     if s.length == 0 then ().embed
     else (char(s(0)), string(s.substring(1))).mapWith((_, _) => ())
 
-  def sepBy[A](p: Parser[A], sep: Parser[Any]): Parser[List[A]] = ???
+  extension [X](p: Parser[X])
+    def optional: Parser[Option[X]] = p.recoverable.map(Some(_)) <|> None.embed
 
-  def sepBy1[A](p: Parser[A], sep: Parser[Any]): Parser[List[A]] = ???
+    def sepBy(sep: Parser[Any]): Parser[List[X]] =
+      sepBy1(sep).recoverable <|> Nil.inject
+
+    def sepBy1(sep: Parser[Any]): Parser[List[X]] = ???
 
