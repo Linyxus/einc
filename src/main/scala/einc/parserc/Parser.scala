18dea5fc520f7b50332b621d27bf3294637271a8
Yichen Xu <yichen.x@outlook.com>
2024-01-03 12:37:15 +0100

    Refactor parser

--- src/main/scala/einc/parserc/Parser.scala
+++ src/main/scala/einc/parserc/Parser.scala
@@ -2,18 +2,45 @@ package einc.parserc
 
 import MonadOps.*
 
-case class SourcePos(pos: Int)
+case class SourcePos(pos: Int):
+  def next: SourcePos = SourcePos(pos + 1)
 
-case class ParseInput(source: String, current: SourcePos)
+case class ParseInput(source: String, current: SourcePos):
+  def currentChar: Option[Char] =
+    if current.pos < source.length then Some(source(current.pos))
+    else None
+
+  def forward: ParseInput = ParseInput(source, current.next)
+
+  override def toString(): String =
+    val (prefix, suffix) = source.splitAt(current.pos)
+    s"SourcePos($prefix|$suffix)"
+
+object SourcePos:
+  def init: SourcePos = SourcePos(0)
 
 case class ParseContext(descs: List[String]):
   def withDesc(desc: String): ParseContext = ParseContext(desc :: descs)
 
+object ParseContext:
+  def empty: ParseContext = ParseContext(Nil)
+
 def ctx(using ctx: ParseContext): ParseContext = ctx
 
-sealed trait ParseOutcome[+X]
+sealed trait ParseOutcome[+X]:
+  def addAlts(alts: List[ParseError]): ParseOutcome[X] = this match
+    case err @ ParseError(msg, pos, alts1) => err.derivedParseError(msg, pos, alts1 ++ alts)
+    case res @ ParseOk(x, next, altErrors) => res.copy(altErrors = altErrors ++ alts)
+
 case class ParseError(msg: String, pos: SourcePos, alts: List[ParseError])(using ctx: ParseContext) extends ParseOutcome[Nothing]:
   val descs: List[String] = ctx.descs
+
+  override def toString(): String =
+    s"ParseError($msg, $pos, $alts) under $descs"
+
+  def derivedParseError(msg1: String, pos1: SourcePos, alts1: List[ParseError]): ParseError =
+    ParseError(msg1, pos1, alts1)
+
 case class ParseOk[X](x: X, next: ParseInput, altErrors: List[ParseError]) extends ParseOutcome[X]
 
 trait Parser[+X]:
@@ -33,13 +60,35 @@ object Parser:
   def flatten[X](ppa: Parser[Parser[X]]): Parser[X] = Parser: input =>
     ppa.run(input) match
       case e @ ParseError(ctx, pos, alts) => e
-      case ParseOk(x, next, altErrors) => ???
+      case ParseOk(pa, input1, altErrors1) => pa.run(input1) match
+        case e @ ParseError(ctx, pos, alts) => ParseError(ctx, pos, altErrors1 ++ alts)
+        case ParseOk(x, next, altErrors2) => ParseOk(x, next, altErrors1 ++ altErrors2)
 
-  given parserIsMonad: Monad[Parser] with
-    def unit[X](x: X): Parser[X] = Parser: i =>
+  given parserIsApplicative: Applicative[Parser] with
+    def pure[X](x: X): Parser[X] = Parser: i =>
       ParseOk(x, i, Nil)
 
-    extension [A](fa: Parser[A])
-      def flatMap[B](op: A => Parser[B]): Parser[B] = ???
+    extension [A](fa: Parser[A]) def map[B](op: A => B): Parser[B] = Parser: i =>
+      fa.run(i) match
+        case err @ ParseError(msg, pos, alts) => err
+        case ParseOk(x, next, altErrors) => ParseOk(op(x), next, altErrors)
 
-  given parserIsApplicative: Applicative[Parser] = parserIsMonad.asApplicative
+    extension [A, B](ff: Parser[A => B]) def <*>(fa: => Parser[A]): Parser[B] =
+      ff.flatMap: f =>
+        fa.map(f)
+
+  given parserIsMonad: Monad[Parser] with
+    def unit[X](x: X): Parser[X] = x.embed
+
+    extension [A](fa: Parser[A])
+      def flatMap[B](op: A => Parser[B]): Parser[B] = flatten(op <#> fa)
+
+
+  given parserIsAlternative: Alternative[Parser] with
+    def fail[X]: Parser[X] = Parser.fail("none of the alternative matches")
+
+    extension [A](pa: Parser[A]) 
+      def <|>[B](pb: => Parser[B]): Parser[A | B] = Parser: input =>
+        pa.run(input) match
+          case err @ ParseError(msg, pos, alts1) => pb.run(input).addAlts(err :: alts1)
+          case res @ ParseOk(x, next, altErrors) => res
